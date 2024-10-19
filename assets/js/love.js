7c9e2c248719ef2d27c09a6bd5b48702cfb302f6
PhongLB_2004 <luongphong.dev@gmail.com>
2024-10-19 21:41:27 +0700

    feat: music, title

--- assets/js/love.js
+++ assets/js/love.js
@@ -186,7 +186,7 @@
       ctx.moveTo(0, 0);
       ctx.scale(0.75, 0.75);
       ctx.font = "12px 微软雅黑,Verdana";
-      ctx.fillText("Click here", 23, 10);
+      ctx.fillText("Click heart", 23, 10);
       ctx.restore();
     },
     clear: function () {
